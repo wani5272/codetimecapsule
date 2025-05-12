be58d4d1e66f1dcda64d77571d7c00170b8fce9f
wani527 <205021230+wani5272@users.noreply.github.com>
2025-05-12 13:12:29 +0000

    commit

---
+++ cbook/code/mdimarr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char board[3][3] = {{'1', '-', '-'},
+                        {'2', '-', '-'},
+                        {'3', '-', '-'}};
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf ("\n");
+    }
+    return EXIT_SUCCESS;
+}
